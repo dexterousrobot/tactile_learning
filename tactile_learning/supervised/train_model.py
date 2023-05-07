3301b50dc41eacfe288cea2720ad395fa18549e7
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-05-07 07:26:58 +0100

    working version

--- tactile_learning/supervised/train_model.py
+++ tactile_learning/supervised/train_model.py
@@ -1,7 +1,6 @@
 from tactile_learning.supervised.simple_train_model import simple_train_model
 from tactile_learning.supervised.train_model_w_metrics import train_model_w_metrics
-from tactile_learning.supervised.train_mdn_jl_model import train_mdn_jl_model
-from tactile_learning.supervised.train_mdn_ac_model import train_mdn_ac_model
+from tactile_learning.supervised.train_mdn_model import train_mdn_model
 
 def train_model(
     prediction_mode,
@@ -16,20 +15,8 @@ def train_model(
     device='cpu'
 ):
 
-    if model.__class__.__name__ in ['MDN_JL']:
-        val_loss, train_time = train_mdn_jl_model(
-            prediction_mode,
-            model,
-            label_encoder,
-            train_generator,
-            val_generator,
-            learning_params,
-            save_dir,
-            device=device
-        )
-
-    elif model.__class__.__name__ in ['MDN_AC']:
-        val_loss, train_time = train_mdn_ac_model(
+    if model.__class__.__name__ in ['MDN_JL', 'MDN_AC']:
+        val_loss, train_time = train_mdn_model(
             prediction_mode,
             model,
             label_encoder,

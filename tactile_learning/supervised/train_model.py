02a0e09b1736098a99670df4a864f34c12612b68
Nathan Lepora <n.lepora@bristol.ac.uk>
2023-05-09 20:38:55 +0100

    simplified and combined train_model

--- tactile_learning/supervised/train_model.py
+++ tactile_learning/supervised/train_model.py
@@ -1,9 +1,7 @@
 from tactile_learning.supervised.simple_train_model import simple_train_model
-from tactile_learning.supervised.train_mdn_model import train_mdn_model
 
 
 def train_model(
-    prediction_mode,
     model,
     label_encoder,
     train_generator,
@@ -13,28 +11,14 @@ def train_model(
     device='cpu'
 ):
 
-    if model.__class__.__name__ in ['MDN_JL', 'MDN_AC']:
-        val_loss, train_time = train_mdn_model(
-            prediction_mode,
-            model,
-            label_encoder,
-            train_generator,
-            val_generator,
-            learning_params,
-            save_dir,
-            device=device
-        )
-
-    else:
-        val_loss, train_time = simple_train_model(
-            prediction_mode,
-            model,
-            label_encoder,
-            train_generator,
-            val_generator,
-            learning_params,
-            save_dir,
-            device=device
-        )
+    val_loss, train_time = simple_train_model(
+        model,
+        label_encoder,
+        train_generator,
+        val_generator,
+        learning_params,
+        save_dir,
+        device=device
+    )
 
     return val_loss, train_time
